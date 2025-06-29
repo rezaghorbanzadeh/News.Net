cfee3ed775362134e40e85afcfe6aa136dc4fbc4
reza <rdaqrbanzadh206@gmail.com>
2025-06-29 19:33:51 +0330

    add view

--- Areas/PanelAdmin/Controllers/PostsController.cs
+++ Areas/PanelAdmin/Controllers/PostsController.cs
@@ -2,11 +2,43 @@
 
 namespace NewsSite.Areas.PanelAdmin.Controllers
 {
-    public class PostsController : Controller
+    public class PostsController : AdminPanelController
     {
-        public IActionResult Index()
+        #region Constructor
+
+        #endregion
+
+        #region PostList
+        [HttpGet("Panel/PostList")]
+        public IActionResult PostList()
         {
             return View();
         }
+        #endregion  
+        
+        
+        #region Create Post
+        [HttpGet("Panel/CreatePost")]
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+        #endregion
+        
+        #region Edit Post
+        [HttpGet("Panel/EditPost")]
+        public IActionResult EditPost()
+        {
+            return View();
+        }
+        #endregion   
+        
+        #region Delete Post
+        [HttpGet("Panel/DeletePost")]
+        public IActionResult DeletePost()
+        {
+            return View();
+        }
+        #endregion
     }
 }
